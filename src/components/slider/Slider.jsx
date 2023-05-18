e664b1f31251f06b32e9e96b792200a7a44dba88
praisex64 <akpevweess1@gmail.com>
2023-05-18 10:00:36 +0100

    feat: rendering of image to view

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,12 +1,42 @@
-
+import { useState, useEffect} from 'react'; 
 import React from 'react'; 
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from 'react-icons/ai'
+import {sliderData} from "./slider-data"; 
+
 
 
 const Slider = () => {
 
+    const [currentSlide, setCurrentSlide] = useState(0); 
+
     return (
         <div className="slider">
-               <h1>Slider</h1> 
+            <AiOutlineArrowLeft className="arrow prev" />
+            <AiOutlineArrowRight className="arrow next" />
+
+            {sliderData.map((slide, index) => {
+
+                    return (
+                        <div className={index === currentSlide ? "slide current" : "slide"} key={index}>
+                        {index === currentSlide && (
+                            <>
+                            <img src={slide.image} alt="slide" />
+                            <div className="content">
+                                <h2>{slide.heading}</h2>
+                                <p>{slide.desc}</p>
+                                <hr /> 
+
+                                <button>Get Started</button>
+                            </div>
+                            </>
+                        )}
+                        </div>
+                    )
+
+                
+            })}
+
+               
         </div>
     )
 
