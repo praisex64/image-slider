cff4f75626218fb7f45e38ad3050e05f756e2d05
praisex64 <akpevweess1@gmail.com>
2023-05-18 09:28:30 +0100

    image slider in react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Slider from "./components/slider/Slider"
+
+
+function App() {
+ 
+  return (
+    <>
+      <Slider />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'; 
+
+
+const Slider = () => {
+
+    return (
+        <div className="slider">
+               <h1>Slider</h1> 
+        </div>
+    )
+
+}
+
+export default Slider; 
